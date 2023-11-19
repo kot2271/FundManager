0cd37c9d13477be7d47b425e00445574435196f6
kotan <kot2271@yandex.ru>
2023-11-19 14:28:39 +0300

    added formatting of contracts.

--- contracts/Foundation.sol
+++ contracts/Foundation.sol
@@ -15,33 +15,40 @@ contract Foundation is Ownable {
 
     //This error is raised when the contract does not have enough balance to send the requested amount.
     error InsufficientBalance(uint256 available, uint256 required);
-    
+
     //This error is raised when the donation amount is zero or negative.
     error DonationAmountMustBeGreaterThanZero();
 
     //initializes the receiver and description variables.
-    constructor(address payable _receiver, string memory _description) Ownable(msg.sender) {
+    constructor(
+        address payable _receiver,
+        string memory _description
+    ) Ownable(msg.sender) {
         receiver = _receiver;
         description = _description;
     }
 
     /**
-     * This function allows anyone to donate funds to the foundation. 
+     * This function allows anyone to donate funds to the foundation.
      * It emits a FundReceived event with the address of the donor and the amount donated.
      */
     function donate() external payable {
-        if(msg.value <= 0) revert DonationAmountMustBeGreaterThanZero();
+        if (msg.value <= 0) revert DonationAmountMustBeGreaterThanZero();
         emit FundReceived(msg.sender, msg.value);
     }
 
     /**
-     * This function allows the owner of the contract to send funds to the receiver. 
-     * It checks if there is enough balance in the contract before sending the funds. 
+     * This function allows the owner of the contract to send funds to the receiver.
+     * It checks if there is enough balance in the contract before sending the funds.
      * It emits a FundsSent event with the address of the receiver and the amount sent.
      * @param amount the amount of funds that will go to the receiver.
      */
     function sendFunds(uint256 amount) external onlyOwner {
-        if (address(this).balance < amount) revert InsufficientBalance({available: address(this).balance, required: amount});
+        if (address(this).balance < amount)
+            revert InsufficientBalance({
+                available: address(this).balance,
+                required: amount
+            });
         (bool sent, ) = receiver.call{value: amount}("");
         if (sent) emit FundsSent(receiver, amount);
     }
